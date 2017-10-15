87827db0f0ef3c76e3835382fe2a72fb480c96cc
Juho Kaasalainen <juho@taiste.fi>
2017-10-15 12:30:28 +0300

    Added project dir hierarchy

---
+++ arduino/bluetooth.ino
@@ -0,0 +1,27 @@
+//http://howtomechatronics.com/tutorials/arduino/arduino-and-hc-05-bluetooth-module-tutorial/
+
+#define ledPin 7
+int state = 0;
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, LOW);
+  Serial.begin(9600);
+}
+
+void loop() {
+  if (Serial.available() > 0) {
+    state = Serial.read();
+  }
+
+  if (state == '1') {
+    digitalWrite(ledPin, LOW);
+    Serial.println("LED: OFF");
+    state = 0;
+  }
+  else if (state == '2') {
+    digitalWrite(ledPin, HIGH);
+    Serial.println("LED: ON");
+    state = 0;
+  }
+}
